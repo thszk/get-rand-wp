9e9fcd31cba52f4a363f245108690d04b7f954f8
Thiago S <31439064+thszk@users.noreply.github.com>
2019-07-01 23:55:30 -0400

    update script

--- randomWallpaper.sh
+++ randomWallpaper.sh
@@ -1,8 +1,38 @@
 #! /usr/bin/env bash
 
-lynx -source https://wallhaven.cc/random |
-  sed 's/a class="preview" href="/\n/g' |
-    sed '/^</d' |
-      sed 's/"  target="_blank".*src="" ><//g' |
-        sed 's/"  .*//'
-echo "\n"
+# SFW content
+SFW() {
+    lynx -source https://wallhaven.cc/random |
+      sed 's/a class="preview" href="/\n/g' |
+        sed '/^</d' |
+          sed 's/"  target="_blank".*src="" ><//g' |
+            sed 's/"  .*//' > file
+}
+
+# NSFW content
+NSFW(){
+    lynx -source 'https://wallhaven.cc/search?purity=010&sorting=random' |
+      sed 's/a class="preview" href="/\n/g' |
+        sed '/^</d' |
+          sed 's/"  target="_blank".*src="" ><//g' |
+            sed 's/"  .*//' > file
+}
+
+# automatically open links
+OPEN() {
+    while read line; do
+        firefox --new-tab "$line"
+        sleep 0.5
+    done < file
+}
+
+# main
+case "$1" in
+    -n) NSFW ;;
+    *) SFW ;;
+esac
+
+# OPEN
+cat file
+echo ""
+rm file
